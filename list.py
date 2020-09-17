ee60a3ea540e8a6fe7880ee4264cc0e8c5e12524
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-09-17 14:11:24 +0300

    Create list.py

---
+++ list.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append("/mnt/volume/packages/venv/lib/python3.8/site-packages/")
+
+import json
+import boto3
+import os
+
+def Main(rq):
+    print(rq)
+    
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net',
+        aws_access_key_id=os.environ['key_id'],
+        aws_secret_access_key=os.environ['access_key'],
+        )
+    
+    # Получаем данные
+    messagelist = dict()
+    
+    for key in s3.list_objects(Bucket=os.environ['bucket'])['Contents']:
+        get_object_response = s3.get_object(
+            Bucket=os.environ['bucket'], Key=key['Key'])
+        messagelist[key['Key']] = json.loads(
+            get_object_response['Body'].read().decode())
+
+    return messagelist, None
