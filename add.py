884e4dfdc1d480b700a972ac328db107c4ae3267
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-09-17 15:20:14 +0300

    add second function

--- add.py
+++ add.py
@@ -2,13 +2,15 @@ import sys
 
 sys.path.append("/mnt/volume/packages/venv/lib/python3.8/site-packages/")
 
-import json
-import boto3
 import os
+import boto3
+import json
+
 
 def Main(rq):
+
     #print(rq)
-    
+
     session = boto3.session.Session()
     s3 = session.client(
         service_name='s3',
@@ -16,15 +18,12 @@ def Main(rq):
         endpoint_url='https://s3.rusonyx.ru',
         aws_access_key_id=os.environ['key_id'],
         aws_secret_access_key=os.environ['access_key'],
-        )
-    
-    # Получаем данные
-    messagelist = dict()
-    
-    for key in s3.list_objects(Bucket=os.environ['bucket'])['Contents']:
-        get_object_response = s3.get_object(
-            Bucket=os.environ['bucket'], Key=key['Key'])
-        messagelist[key['Key']] = json.loads(
-            get_object_response['Body'].read().decode())
+    )
 
-    return messagelist, None
+    # Загрузить объекты в бакет
+
+    ## Из строки
+    s3.put_object(Bucket=os.environ['bucket'], Key=rq['args']['email'], Body=str(
+        json.dumps(rq['args'])))
+
+    return rq, None
